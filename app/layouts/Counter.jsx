99621dbbd447d3c8ead966c9d0f5a2e110374328
menabil <nabil99cc@gmail.com>
2026-02-20 22:18:37 +0600

    add counter part

--- app/layouts/Counter.jsx
+++ app/layouts/Counter.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import Container from '../components/Container'
+import React from "react";
+import Container from "../components/Container";
+import Flex from "../components/Flex";
 
 const Counter = () => {
   return (
-    <div className='my-32.5'>
-        <Container>
-
-        </Container>
+    <div className="my-32.5">
+      <Container>
+        <Flex></Flex>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
